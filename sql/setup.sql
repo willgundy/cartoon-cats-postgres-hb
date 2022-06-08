c55d148128dfaaf40ce1f3715b8c1032a5effedf
Will Gundy <willgunderson503@gmail.com>
2022-06-08 12:03:53 -0700

    initial commit with setup.sql completed

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,18 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+
+DROP table if exists cats;
+
+CREATE TABLE cats (
+    id BIGINT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR NOT NULL,
+    age INT NOT NULL
+)
+
+INSERT INTO cats (name, age) VALUES
+('James Corden', 43),
+('Taylor Swift', 32),
+('Dame Judi Dench', 87),
+('Rebel Wilson', 42),
+('Idris Elba', 49),
+('Jennifer Hudson', 40);
